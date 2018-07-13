38c6fb108ec847091c391013ca5c14e8ed9326c5
sirromdev <sirromdev@gmail.com>
2018-07-13 13:46:35 +0100

    Detecting web3 account changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 import getWeb3 from './utils/getWeb3'
-
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
+
+// const Splitter = require("../build/contracts/Splitter.json")
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      storageValue: 0,
-      web3: null
+      web3: null,
+      split: null,
+      address: "",
+      accounts: [],
+      currentAccount: "",
+      balance: "",
+      network: ""
     }
+    this.handleAddress= this.handleAddress.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
@@ -22,12 +30,67 @@ class App extends Component {
       this.setState({
         web3: results.web3
       })
+      this.getCurrent()
     })
     .catch(() => {
       console.log('Error finding web3.')
     })
   }
-  
+
+  handleAddress(event) {
+    this.setState({
+        address: event.target.value
+    })  
+  }
+
+  handleSubmit() {
+    if(this.state.web3.utils.isAddress(this.state.address)) {
+      var acc = this.state.accounts
+      acc.push(this.state.address)
+
+      this.setState({
+        accounts: acc,
+        address: "",
+      })
+    }
+  }
+
+  fetchAccounts() {
+    if (this.props.web3 !== null) {
+      this.state.web3.eth.getAccounts((err, accounts) => {
+        if (err) {
+          console.log(err)
+        } 
+        else {
+          if (accounts.length === 0) {
+            this.setState({
+              currentAccount: "locked",
+              balance: "locked"
+            })
+          } 
+          else {
+            if (accounts[0] !== this.state.account) {
+              this.state.web3.eth.getAccounts((err, accounts) => {
+                this.state.web3.eth.getBalance(accounts[0], (err, balance) => {
+                  this.setState({
+                    currentAccount: accounts[0],
+                    balance: balance
+                  })
+                }); 
+              })
+            }
+          }
+        }
+      });
+    }
+  }
+
+  getCurrent() {
+    let self = this;
+    this.fetchAccounts()
+    self.AccountInterval = setInterval(() => self.fetchAccounts(), 1000);
+  }
+
   render() {
     return (
       <div className="App">
@@ -38,16 +101,29 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-3">
+            <br/>
+            Current Account: {this.state.currentAccount}
+            <br/>
+            Balance: {this.state.balance}
+            <br/>
+            Network:
+
             </div>
             <div className="pure-u-1-3">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
+            <br/>
+            <form className="pure-form">
+              <fieldset>
+                <input className="pure-input-2-3" type="text" value={this.state.address} onChange={this.handleAddress} placeholder="0x1234"/>
+                <a type="submit" className="pure-button pure-button-primary" onClick={this.handleSubmit}>Add Payee</a>
+              </fieldset>
+            </form>
+
             </div>
-            <div className="pure-u-1-3">
-            </div>
+            <div className="pure-u-1-3"></div>
           </div>
+
+          
+
         </main>
       </div>
     );
